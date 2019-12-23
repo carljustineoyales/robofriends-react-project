d153c5bc73643a883e85da3d1732e585e00561e1
carl justine oyales <51884760+carljustineoyales@users.noreply.github.com>
2019-12-23 18:37:50 +0800

    Updates

---
+++ precache-manifest.03bb9feab8470a1870d9a40f0cf9986e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3f6382d8ad7fe1beb118a51a76b70b",
+    "url": "/robofriends-react-project/index.html"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/robofriends-react-project/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "44c8a5037ec20e5125e0",
+    "url": "/robofriends-react-project/static/css/main.129217fc.chunk.css"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/robofriends-react-project/static/js/2.f0d44eb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends-react-project/static/js/2.f0d44eb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44c8a5037ec20e5125e0",
+    "url": "/robofriends-react-project/static/js/main.c10c5c5b.chunk.js"
+  },
+  {
+    "revision": "e05ed7eee9ea20bfcda1",
+    "url": "/robofriends-react-project/static/js/runtime-main.2c329c50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react-project/static/media/SEGA.12a3db8b.woff"
+  }
+]);
